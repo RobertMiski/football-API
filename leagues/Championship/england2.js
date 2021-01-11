541c4c7270e2c5bbb830dce11f164de20a319cbc
RobertMiski <72018451+RobertMiski@users.noreply.github.com>
2021-01-11 17:56:25 +0000

    Revert "d"
    This reverts commit b44d73a66e9357f8b5a5dc3294e061f2156967ac.

--- leagues/Championship/england2.js
+++ leagues/Championship/england2.js
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
       let template1 = `
       <tr class="manele2 blue darken-2">
-        <td class="valoare">${position} <button class="btn-floating btn-small"><i class="material-icons blue">arrow_upward</i></button></td>
+        <td class="valoare">${position} <button class="btn-floating btn-small blue"><i class="material-icons blue">arrow_upward</i></button></td>
         <td><img src="${imgurl}" height="20px" width="20px">${name}</td>
         <td class = "center-align hide-on-med-and-down">${form};</td>
         <td class="center-align">${playedGames}</td>
@@ -45,7 +45,7 @@ document.addEventListener('DOMContentLoaded', function() {
       `;
       let template2 = `
       <tr class="manele3 red lighten-3">
-        <td class="valoare">${position} <button class="btn-floating btn-small"><i class="material-icons red">arrow_downward</i></button></td>
+        <td class="valoare">${position} <button class="btn-floating btn-small red"><i class="material-icons red">arrow_downward</i></button></td>
         <td><img src="${imgurl}" height="20px" width="20px">${name}</td>
         <td class = "center-align hide-on-med-and-down">${form};</td>
         <td class="center-align">${playedGames}</td>
@@ -58,7 +58,7 @@ document.addEventListener('DOMContentLoaded', function() {
   
       let template3= `
       <tr class="manele3 brown lighten-2">
-        <td class="valoare">${position} <button class="btn-floating btn-small playoff"></button></td>
+        <td class="valoare">${position} 
         <td><img src="${imgurl}" height="20px" width="20px">${name}</td>
         <td class = "center-align hide-on-med-and-down">${form};</td>
         <td class="center-align">${playedGames}</td>
