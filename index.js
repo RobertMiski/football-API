3abd19cb072e9823048400c432985ca2650ce03d
RobertMiski <72018451+RobertMiski@users.noreply.github.com>
2021-01-11 12:53:16 +0000

    bl

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ document.addEventListener('DOMContentLoaded', function() {
   var instances = M.Sidenav.init(elems,instances);
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Dropdown.init(elems, instances);
+});
 let manele = document.querySelector('.manelepara');
 let buton = document.querySelector('.btn-large');
 let standings = document.querySelector('.standings');
