a7684c6f6b11a4fcab5d0b4593ec869624c3a17e
Alex Visan <visanpaypal@gmail.com>
2021-01-11 09:50:00 +0100

    small fixes

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems,instances);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.dropdown-trigger');
     var instances = M.Dropdown.init(elems, instances);
   });
